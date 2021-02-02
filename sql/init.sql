94bdbfdaa5fbb4d39e2be37ac88cef92b41f4010
jiaqing738 <jiaqing738@hotmail.com>
2021-02-03 00:58:51 +0800

    fixed sql insert statement of mockup data

--- sql/init.sql
+++ sql/init.sql
@@ -19,8 +19,8 @@ TABLESPACE pg_default;
 ALTER TABLE booking.booking_config OWNER to facilityadmin;
 
 /*Default Config*/
-INSERT INTO booking.booking_config(key, value) VALUES ('max_hr_per_booking', '2');
-INSERT INTO booking.booking_config(key, value) VALUES ('max_bookahead', '15');
+INSERT INTO booking.booking_config(key, value) VALUES ('max_hr_per_booking', '6');
+INSERT INTO booking.booking_config(key, value) VALUES ('max_bookahead', '14');
 INSERT INTO booking.booking_config(key, value) VALUES ('booking_start_time', '08:00');
 INSERT INTO booking.booking_config(key, value) VALUES ('booking_end_time', '18:00');
 
@@ -75,9 +75,9 @@ INSERT INTO booking.account(user_id, admin, email, password) VALUES ('admin', tr
 INSERT INTO booking.account(user_id, admin, email, password) VALUES ('john', false, 'john@mail.com', crypt('P@ssw0rd123', gen_salt('bf')));
 INSERT INTO booking.account(user_id, admin, email, password) VALUES ('james', false, 'james@mail.com', crypt('P@ssw0rd123', gen_salt('bf')));
 INSERT INTO booking.account(user_id, admin, email, password) VALUES ('peter', false, 'peter@mail.com', crypt('P@ssw0rd123', gen_salt('bf')));
-INSERT INTO booking.facility_detail VALUES (1, 'Meeting Room L1-01', 'L1', 'Meeting Room', 'OPEN', '2021-02-02 13:43:13.158954+00');
-INSERT INTO booking.facility_detail VALUES (2, 'Meeting Room L1-02', 'L1', 'Meeting Room', 'OPEN', '2021-02-02 13:43:33.93617+00');
-INSERT INTO booking.facility_detail VALUES (3, 'Meeting Room L2-01', 'L2', 'Meeting Room', 'MAINTENANCE', '2021-02-02 13:43:57.982175+00');
-INSERT INTO booking.facility_detail VALUES (4, 'Meeting Room L2-02', 'L2', 'Meeting Room', 'OPEN', '2021-02-02 13:44:13.422796+00');
-INSERT INTO booking.facility_detail VALUES (5, 'Training Room L1-01', 'L1', 'Training Room', 'OPEN', '2021-02-02 13:45:12.221773+00');
-INSERT INTO booking.facility_detail VALUES (6, 'Training Room L1-02', 'L1', 'Training Room', 'OPEN', '2021-02-02 13:45:30.445954+00');
+INSERT INTO booking.facility_detail(name, level, description, status, transaction_dt) VALUES ('Meeting Room L1-01', 'L1', 'Meeting Room', 'OPEN', '2021-02-02 13:43:13.158954+00');
+INSERT INTO booking.facility_detail(name, level, description, status, transaction_dt) VALUES ('Meeting Room L1-02', 'L1', 'Meeting Room', 'OPEN', '2021-02-02 13:43:33.93617+00');
+INSERT INTO booking.facility_detail(name, level, description, status, transaction_dt) VALUES ('Meeting Room L2-01', 'L2', 'Meeting Room', 'MAINTENANCE', '2021-02-02 13:43:57.982175+00');
+INSERT INTO booking.facility_detail(name, level, description, status, transaction_dt) VALUES ('Meeting Room L2-02', 'L2', 'Meeting Room', 'OPEN', '2021-02-02 13:44:13.422796+00');
+INSERT INTO booking.facility_detail(name, level, description, status, transaction_dt) VALUES ('Training Room L1-01', 'L1', 'Training Room', 'OPEN', '2021-02-02 13:45:12.221773+00');
+INSERT INTO booking.facility_detail(name, level, description, status, transaction_dt) VALUES ('Training Room L1-02', 'L1', 'Training Room', 'OPEN', '2021-02-02 13:45:30.445954+00');
